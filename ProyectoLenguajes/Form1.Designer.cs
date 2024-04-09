c63c71e686f15516366a62e9edd2d8a812e3b0d4
pabloo-zam <PFZAMORA@Correo.url.edu.gt>
2024-04-09 11:08:19 -0600

    Cambios visuales

--- ProyectoLenguajes/Form1.Designer.cs
+++ ProyectoLenguajes/Form1.Designer.cs
@@ -30,10 +30,14 @@
         {
             this.BtnAbrir = new System.Windows.Forms.Button();
             this.RchMostrar = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtCadena = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // BtnAbrir
             // 
+            this.BtnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnAbrir.Location = new System.Drawing.Point(70, 80);
             this.BtnAbrir.Name = "BtnAbrir";
             this.BtnAbrir.Size = new System.Drawing.Size(75, 23);
@@ -46,21 +50,52 @@
             // 
             this.RchMostrar.Location = new System.Drawing.Point(61, 134);
             this.RchMostrar.Name = "RchMostrar";
-            this.RchMostrar.Size = new System.Drawing.Size(100, 96);
+            this.RchMostrar.Size = new System.Drawing.Size(236, 269);
             this.RchMostrar.TabIndex = 1;
             this.RchMostrar.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccione un archivo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(398, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingrese una cadena";
+            // 
+            // TxtCadena
+            // 
+            this.TxtCadena.Location = new System.Drawing.Point(401, 92);
+            this.TxtCadena.Name = "TxtCadena";
+            this.TxtCadena.Size = new System.Drawing.Size(264, 20);
+            this.TxtCadena.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtCadena);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.RchMostrar);
             this.Controls.Add(this.BtnAbrir);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +103,9 @@
 
         private System.Windows.Forms.Button BtnAbrir;
         private System.Windows.Forms.RichTextBox RchMostrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtCadena;
     }
 }
 
